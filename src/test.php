c1f83fa1ae81ce6b1a9841787a48ca45b58b4332
smiley <codemasher@users.noreply.github.com>
2022-11-25 14:40:47 +0100

    :rainbow_flag:

---
+++ src/test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * test.php
+ *
+ * @created      25.11.2022
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2022 smiley
+ * @license      MIT
+ */
+
+$secret = getenv('TEST_SECRET');
+
+var_dump($secret);
